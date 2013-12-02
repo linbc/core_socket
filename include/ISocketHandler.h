d10372f1afed866b922067b6d8e884e56e2e1dc1
linbc <linbc01@gmail.com>
2013-12-02 14:32:08 +0800

    include/*.h

---
+++ include/ISocketHandler.h
@@ -0,0 +1,168 @@
+/** \file ISocketHandler.h
+ **	\date  2004-02-13
+ **	\author grymse@alhem.net
+**/
+/*
+Copyright (C) 2004-2007  Anders Hedstrom
+
+This library is made available under the terms of the GNU GPL.
+
+If you would like to use this library in a closed-source application,
+a separate license agreement is available. For information about 
+the closed-source license agreement for the C++ sockets library,
+please visit http://www.alhem.net/Sockets/license.html and/or
+email license@alhem.net.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+#ifndef _SOCKETS_ISocketHandler_H
+#define _SOCKETS_ISocketHandler_H
+#include "sockets-config.h"
+
+#include <list>
+
+#include "socket_include.h"
+#include "Socket.h"
+
+#ifdef SOCKETS_NAMESPACE
+namespace SOCKETS_NAMESPACE {
+#endif
+
+typedef enum {
+	LIST_CALLONCONNECT = 0,
+#ifdef ENABLE_DETACH
+	LIST_DETACH,
+#endif
+	LIST_TIMEOUT,
+	LIST_RETRY,
+	LIST_CLOSE
+} list_t;
+
+class SocketAddress;
+class Mutex;
+
+
+/** Socket container class, event generator. 
+	\ingroup basic */
+class ISocketHandler
+{
+	friend class Socket;
+
+public:
+	/** Connection pool class for internal use by the ISocketHandler. 
+		\ingroup internal */
+#ifdef ENABLE_POOL
+	class PoolSocket : public Socket
+	{
+	public:
+		PoolSocket(ISocketHandler& h,Socket *src) : Socket(h) {
+			CopyConnection( src );
+			SetIsClient();
+		}
+
+		void OnRead() {
+			Handler().LogError(this, "OnRead", 0, "data on hibernating socket", LOG_LEVEL_FATAL);
+			SetCloseAndDelete();
+		}
+		void OnOptions(int,int,int,SOCKET) {}
+
+	};
+#endif
+
+public:
+	virtual ~ISocketHandler() {}
+
+	/** Log error to log class for print out / storage. */
+	virtual void LogError(Socket *p,const std::string& user_text,int err,const std::string& sys_err,loglevel_t t = LOG_LEVEL_WARNING) = 0;
+
+	// -------------------------------------------------------------------------
+	// Socket stuff
+	// -------------------------------------------------------------------------
+	/** Add socket instance to socket map. Removal is always automatic. */
+	virtual void Add(Socket *) = 0;
+private:
+	/** Remove socket from socket map, used by Socket class. */
+	virtual void Remove(Socket *) = 0;
+public:
+	/** Get status of read/write/exception file descriptor set for a socket. */
+	virtual void Get(SOCKET s,bool& r,bool& w,bool& e) = 0;
+	/** Set read/write/exception file descriptor sets (fd_set). */
+	virtual void Set(SOCKET s,bool bRead,bool bWrite,bool bException = true) = 0;
+
+	/** Wait for events, generate callbacks. */
+	virtual int Select(long sec,long usec) = 0;
+	/** This method will not return until an event has been detected. */
+	virtual int Select() = 0;
+	/** Wait for events, generate callbacks. */
+	virtual int Select(struct timeval *tsel) = 0;
+
+	/** Check that a socket really is handled by this socket handler. */
+	virtual bool Valid(Socket *) = 0;
+	/** Return number of sockets handled by this handler.  */
+	virtual size_t GetCount() = 0;
+
+	/** Override and return false to deny all incoming connections. 
+		\param p ListenSocket class pointer (use GetPort to identify which one) */
+	virtual bool OkToAccept(Socket *p) = 0;
+
+	/** Called by Socket when a socket changes state. */
+	virtual void AddList(SOCKET s,list_t which_one,bool add) = 0;
+
+	// -------------------------------------------------------------------------
+	// Connection pool
+	// -------------------------------------------------------------------------
+#ifdef ENABLE_POOL
+	/** Find available open connection (used by connection pool). */
+	virtual ISocketHandler::PoolSocket *FindConnection(int type,const std::string& protocol,SocketAddress&) = 0;
+	/** Enable connection pool (by default disabled). */
+	virtual void EnablePool(bool = true) = 0;
+	/** Check pool status. 
+		\return true if connection pool is enabled */
+	virtual bool PoolEnabled() = 0;
+#endif // ENABLE_POOL
+	// -------------------------------------------------------------------------
+	// DNS resolve server
+	// -------------------------------------------------------------------------
+#ifdef ENABLE_RESOLVER
+	/** Enable asynchronous DNS. 
+		\param port Listen port of asynchronous dns server */
+	virtual void EnableResolver(port_t = 16667) = 0;
+	/** Check resolver status.
+		\return true if resolver is enabled */
+	virtual bool ResolverEnabled() = 0;
+	/** Queue a dns request.
+		\param host Hostname to be resolved
+		\param port Port number will be echoed in Socket::OnResolved callback */
+	virtual int Resolve(Socket *,const std::string& host,port_t port) = 0;
+
+	/** Do a reverse dns lookup. */
+	virtual int Resolve(Socket *,ipaddr_t a) = 0;
+
+	/** Get listen port of asynchronous dns server. */
+	virtual port_t GetResolverPort() = 0;
+	/** Resolver thread ready for queries. */
+	virtual bool ResolverReady() = 0;
+	/** Returns true if socket waiting for a resolve event. */
+	virtual bool Resolving(Socket *) = 0;
+#endif // ENABLE_RESOLVER
+};
+
+
+#ifdef SOCKETS_NAMESPACE
+}
+#endif
+
+#endif // _SOCKETS_ISocketHandler_H
+
---
+++ test/gtest_main.cc
@@ -0,0 +1,52 @@
+// Copyright 2006, Google Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// 单元测试主要从微观到宏观，从基础往上进行测试
+// 1）对象操作
+// 2）事件操作及产生
+// 3）事件产生后的回放应用
+// 4）订阅测试
+// 5）对象表的综合测试
+
+#include <iostream>
+#include <gtest/gtest.h>
+#include <string>
+std::string g_mysql_char_info = "192.168.3.2;3306;dev;asdf;char";
+
+int main(int argc, char **argv) {	
+#if _WIN32
+	_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF  );
+	//_CrtSetBreakAlloc(85872);
+#endif
+
+  std::cout << "Running main() from gtest_main.cc\n";
+  testing::InitGoogleTest(&argc, argv);
+  RUN_ALL_TESTS(); 
+  return 0;
+}
